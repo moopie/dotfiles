eb0407ef80d10b9a8781291df7a01a214f7d4aee
Anton Amirian <platypus@lavabit.com>
2012-12-17 17:40:59 +0200

    made a setup script

---
+++ init.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+
+conf = $XDG_HOME_DIR
+
+ln -s awesome $conf/awesome
+ln -s fontconfig $conf/fontconfig
+ln -s zsh $conf/zsh
+
+ln -s vim ~/.vim
+ln -s irssi ~/.irssi
+
+ln -s zsh/zshrc ~/.zshrc
+ln -s vim/vimrc ~/.vimrc
+ln -s xinitrc ~/.xinitrc
+ln -s Xdefaults ~/.Xdefaults
+ln -s gitconfig ~/.gitconfig
