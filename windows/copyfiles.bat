0ddf3ba4d2c44983c17eb7ea4ef9076842547601
Anton Amirian <extuessl@gmail.com>
2015-05-14 00:25:24 +0300

    more changes for windows

--- windows/copyfiles.bat
+++ windows/copyfiles.bat
@@ -10,6 +10,10 @@ set dotfiles="%UserProfile%\dotfiles"
 xcopy /S /I "%dotfiles%\vim" "%UserProfile%\.vim"
 xcopy "%dotfiles%\vim\vimrc" "%UserProfile%\.vimrc"
 
+:: Bash and etc
+xcopy "%dotfiles%\bashrc" "%UserProfile%\.bashrc"
+xcopy "%dotfiles%\inputrc" "%UserProfile%\.inputrc"
+
 :: Copy AutoHotkey
 xcopy "%dotfiles%\windows\AutoHotkey.ahk" "%HOMEDRIVE%%HOMEPATH%\Start Menu\Programs\Startup\AutoHotkey.ahk"
 
