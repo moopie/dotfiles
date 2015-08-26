a03ded3774a8a34d20ed388c5d4681f0598683bd
Anton Amirian <extuessl@gmail.com>
2015-08-27 00:56:31 +0300

    who even reads these commit messages

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -287,7 +287,7 @@ myStartupHook = setWMName "LG3D"
 
 -- Run xmonad with the settings you specify. No need to modify this.
 --
-main = xmonad =<< xmobar defaults
+main = xmonad =<< defaults
 -- A structure containing your configuration settings, overriding
 -- fields in the default config. Any you don't override, will
 -- use the defaults defined in xmonad/XMonad/Config.hs
