9b841e74fd2f9ed782891c03825cbae181b37b99
Anton Amirian <extuessl@gmail.com>
2015-11-30 23:55:09 +0200

    who does even read these comments? they're shit

--- windows/applications.bat
+++ windows/applications.bat
@@ -7,32 +7,7 @@ echo.
 choco install -y^
  conemu^
  git^
- googlechrome^
- notepadplusplus^
- sublimetext3^
- firefox^
  7zip^
  mpv^
  autohotkey^
- filezilla^
- putty^
- kitty.portable^
- python2^
- paint.net^
- wget^
- curl^
- ag^
- winscp^
- wireshark^
- kdiff3^
- greenshot^
- virtualclonedrive^
- mingw^
- launchy^
- sumatrapdf^
- cmake^
- imgburn^
- rdcman^
- everything^
- glogg^
- nmap
+ launchy
