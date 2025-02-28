1b51e899be25ece7ab544c76e91b51d079b82767
Anton Amirian <extuessl@gmail.com>
2025-02-28 22:16:53 +0200

    neovim: defined nvim-cmp twice for no reason

--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -11,7 +11,21 @@ return {
       'hrsh7th/cmp-path',         -- Path completions
       'saadparwaiz1/cmp_luasnip', -- Snippet completions
       'L3MON4D3/LuaSnip'          -- Snippet Engine
-    }
+    },
+    config = function()
+      local cmp = require("cmp")
+      cmp.setup({
+        mapping = cmp.mapping.preset.insert({
+          ["<C-Space>"] = cmp.mapping.complete(),
+          ["<CR>"] = cmp.mapping.confirm({ select = true }),
+        }),
+        sources = cmp.config.sources({
+          { name = "nvim_lsp" },
+          { name = "buffer" },
+          { name = "path" },
+        }),
+      })
+    end,
   },
 
   -- LSP Formatting (autoformat on save)
