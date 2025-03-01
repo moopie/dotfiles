90c742ee26203735d71a79e0d8048452516be0d9
Anton Amirian <extuessl@gmail.com>
2025-03-01 16:24:35 +0200

    neovim: conform is defined twice too, fix that

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -19,8 +19,31 @@ return {
         "python"
       },
       sync_install = false,
-      highlight = { enable = true },
+
+      -- Enable syntax highlighting
+      highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = false,
+      },
+
+      -- Enable incremental selection
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = "gnn",    -- Start selection
+          node_incremental = "grn",  -- Expand selection
+          scope_incremental = "grc", -- Expand scope
+          node_decremental = "grm",  -- Shrink selection
+        },
+      },
+
+      -- Enable indentation
       indent = { enable = true },
+
+      -- Enable folding with Tree-sitter
+      fold = {
+        enable = true
+      }
     })
   end,
   build = ":TSUpdate"
