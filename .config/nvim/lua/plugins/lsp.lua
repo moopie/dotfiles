78b7101faf10412391c64fa9d293a4e3b59ede37
Anton Amirian <extuessl@gmail.com>
2025-04-03 23:34:04 +0300

    neovim: add gitsigns.lua

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,3 +1,18 @@
 return {
-  'neovim/nvim-lspconfig',
+  "neovim/nvim-lspconfig",
+  opts = {
+    servers = {
+      gopls = {
+        settings = {
+          gopls = {
+            analyses = {
+              unusedparams = true,
+              shadow = true,
+            },
+            staticcheck = true,
+          },
+        },
+      },
+    },
+  },
 }
