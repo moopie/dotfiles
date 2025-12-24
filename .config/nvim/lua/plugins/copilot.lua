a1ffffd1fc8ce0822b83e80488de4a6499e3a9cc
Anton Amirian <extuessl@gmail.com>
2025-12-24 22:19:47 +0200

    neovim: disable copilot

--- .config/nvim/lua/plugins/copilot.lua
+++ .config/nvim/lua/plugins/copilot.lua
@@ -1,36 +1,36 @@
-return {
-  'zbirenbaum/copilot.lua',
-  cmd = "Copilot",
-  build = ":Copilot auth",
-  event = "InsertEnter",
-  config = function()
-    require("copilot").setup({
-      suggestion = {
-        enabled = true,
-        auto_trigger = true,
-        keymap = {
-          accept = "<Tab>",
-          next = "<C-j>",
-          prev = "<C-k>",
-          dismiss = "<C-\\>",
-        },
-      },
-      filetypes = {
-        markdown = true,
-        help = true,
-      },
-      panel = {
-        enabled = true,
-        auto_refresh = true,
-        keymap = {
-          jump_prev = "[[",
-          jump_next = "]]",
-          accept = "<CR>",
-          refresh = "gr",
-          open = "<C-CR>",
-          close = "<C-\\>",
-        },
-      },
-    })
-  end,
-}
+--return {
+--  'zbirenbaum/copilot.lua',
+--  cmd = "Copilot",
+--  build = ":Copilot auth",
+--  event = "InsertEnter",
+--  config = function()
+--    require("copilot").setup({
+--      suggestion = {
+--        enabled = true,
+--        auto_trigger = true,
+--        keymap = {
+--          accept = "<Tab>",
+--          next = "<C-j>",
+--          prev = "<C-k>",
+--          dismiss = "<C-\\>",
+--        },
+--      },
+--      filetypes = {
+--        markdown = true,
+--        help = true,
+--      },
+--      panel = {
+--        enabled = true,
+--        auto_refresh = true,
+--        keymap = {
+--          jump_prev = "[[",
+--          jump_next = "]]",
+--          accept = "<CR>",
+--          refresh = "gr",
+--          open = "<C-CR>",
+--          close = "<C-\\>",
+--        },
+--      },
+--    })
+--  end,
+--}
